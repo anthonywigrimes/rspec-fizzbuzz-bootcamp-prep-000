8ae84d7602ac2cdbee0c0e994411f299cae715e8
Anthony Grimes <agrimes@charter.net>
2017-09-28 02:31:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,3 @@
-def fizzbuzz(num)
-   if num % 5 == 0 && num % 3 == 0
-      "FizzBuzz"
-   elsif num % 5 == 0
-      "Buzz"
-   elsif num % 3 == 0
-      "Fizz"
-   else
-      nil
-   end
+def fizzbuzz
+
 end
